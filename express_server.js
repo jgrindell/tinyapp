ee8ea6198b429b5381000c2e41c351d5a50e2daf
James Grindell <james.grindell@gmail.com>
2020-12-07 15:55:36 -0600

    added delete buttons using POST

--- express_server.js
+++ express_server.js
@@ -23,14 +23,6 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1; res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
-
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
@@ -39,6 +31,12 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+})
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const DeleteURL = req.params['shortURL'];
+  delete urlDatabase[DeleteURL];
+  res.redirect("/urls");
 })
 
 app.listen(PORT, () => {
