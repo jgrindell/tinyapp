797f32f3ff8a17b5ca59f651459c7e7f5beab099
James Grindell <james.grindell@gmail.com>
2020-12-08 17:39:52 -0600

    added login/logout cookie functions

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded());
 
 const urlDatabase = {
   "b2xVn2":"http://www.lighthouselabs.ca",
@@ -14,23 +18,28 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars ={ urls: urlDatabase };  
-    res.render("urls_index", templateVars);
-  }
-);
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"]
+  };  
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  const templateVars = {
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  }
+  res.redirect(templateVars.longURL);
 })
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -42,6 +51,17 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:id", (req, res) => {
   const EditURL = req.params['id'];
   res.redirect(EditURL);
+})
+
+app.post("/login", (req, res) => {
+  const login = req.body;
+  res.cookie('username', login.username);
+  res.redirect('urls');
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('urls');
 })
 
 app.listen(PORT, () => {
